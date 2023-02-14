12e369ae7574ef613117873d334d008c1f684fd1
skips0skips <skips0skips@mail.ru>
2023-02-14 15:37:25 +0300

    Ver. 2

--- visual_odometry/visual_odometry.py
+++ visual_odometry/visual_odometry.py
@@ -72,42 +72,35 @@ class VisualOdometry():
     @staticmethod
     def _form_transf(R, t):
         """
-        Makes a transformation matrix from the given rotation matrix and translation vector
-
-        Parameters
-        ----------
-        R (ndarray): The rotation matrix
-        t (list): The translation vector
-
-        Returns
-        -------
-        T (ndarray): The transformation matrix
+        Функция создает матрицу преобразования из заданной матрицы поворота и вектора перемещения
+        Параметры:
+        R (ndarray): Матрица вращения
+        t (list): Вектор перевода
+        Возвращает:
+        T (ndarray): Матрица преобразования
         """
-        T = np.eye(4, dtype=np.float64)
+        # двумерный массив у которого все элементы по диагонали равны 1, а все остальные равны 0
+        T = np.eye(4, dtype=np.float64) # 4 -количество строк выходного массива
         T[:3, :3] = R
         T[:3, 3] = t
         return T
 
     def get_matches(self, i):
         """
-        This function detect and compute keypoints and descriptors from the i-1'th and i'th image using the class orb object
-
-        Parameters
-        ----------
-        i (int): The current frame
-
-        Returns
-        -------
-        q1 (ndarray): The good keypoints matches position in i-1'th image
-        q2 (ndarray): The good keypoints matches position in i'th image
+        Эта функция обнаруживает и вычисляет ключевые точки, дескриптор из i-1-го и i изображений, используя класс или объект
+        Параметры:
+        i - Текущий кадр (int)
+        Возвращает:
+        q1 (ndarray): Хорошие ключевые точки соответствия положения на i-1-м изображении
+        q2 (ndarray): Хорошие ключевые точки соответствия положения на i изображении
         """
-        # Find the keypoints and descriptors with ORB
+        # Нахождение ключевых точек и дескрипторов с помощью ORB
         kp1, des1 = self.orb.detectAndCompute(self.images[i - 1], None)
         kp2, des2 = self.orb.detectAndCompute(self.images[i], None)
-        # Find matches
+        # Нахождение совпадений, где k число лучших совпадений для каждого дескриптора
         matches = self.flann.knnMatch(des1, des2, k=2)
 
-        # Find the matches there do not have a to high distance
+        # Нахождение совпадений, которые не имеют большого расстояния
         good = []
         try:
             for m, n in matches:
@@ -116,89 +109,88 @@ class VisualOdometry():
         except ValueError:
             pass
 
-        draw_params = dict(matchColor = -1, # draw matches in green color
-                 singlePointColor = None,
-                 matchesMask = None, # draw only inliers
-                 flags = 2)
-
-        img3 = cv2.drawMatches(self.images[i], kp1, self.images[i-1],kp2, good ,None,**draw_params)
+        draw_params = dict(matchColor = -1, # обрисовкасоединяющих линий зеленым цветом
+                 singlePointColor = None, #Цвет отдельных ключевых точек (кружков), что означает, что ключевые точки не имеют совпадений
+                 matchesMask = None, # Маска, определяющая, какие совпадения будут нарисованы. Если маска пуста, все совпадения отображаются
+                 flags = 2) #Флаги, устанавливающие функции рисования
+        
+        # Рисует найденные совпадения ключевых точек из двух изображений.
+        # images[i], images[i-1] - первое и второе исходное изображение
+        # kp1, kp2 - ключевые точки из первого и второго исходного изображения
+        # good - список точек соответствия первого и воторого изображения
+        # outImg - вывод изображения
+        img3 = cv2.drawMatches(self.images[i], kp1, self.images[i-1],kp2, good ,outImg = None,**draw_params)
         cv2.imshow("image", img3)
         cv2.waitKey(200)
 
-        # Get the image points form the good matches
+        # Получение списока точек соответствия первого и воторого изображения
         q1 = np.float32([kp1[m.queryIdx].pt for m in good])
         q2 = np.float32([kp2[m.trainIdx].pt for m in good])
         return q1, q2
 
     def get_pose(self, q1, q2):
         """
-        Calculates the transformation matrix
-
-        Parameters
-        ----------
-        q1 (ndarray): The good keypoints matches position in i-1'th image
-        q2 (ndarray): The good keypoints matches position in i'th image
-
-        Returns
-        -------
-        transformation_matrix (ndarray): The transformation matrix
+        Функция вычисляет матрицу преобразования
+        Параметры:
+        q1 (ndarray): Хорошие ключевые точки соответствия положения на i-1-м изображении
+        q2 (ndarray): Хорошие ключевые точки соответствия положения на i изображении
+        Возращает:
+        transformation_matrix (ndarray) - Матрица преобразования
         """
-        # Essential matrix
+        # Вычисляет существенную матрицу из  точек на двух изображениях.
+        #threshold - максимальное расстояние от точки до эпиполярной линии в пикселях, за пределами которого точка считается выбросом
         E, _ = cv2.findEssentialMat(q1, q2, self.K, threshold=1)
 
-        # Decompose the Essential matrix into R and t
+        # Разложение существенной матрицы на ветор перемещения и матрицу поворота
         R, t = self.decomp_essential_mat(E, q1, q2)
 
-        # Get transformation matrix
+        # Получаем матрицу преобразования
         transformation_matrix = self._form_transf(R, np.squeeze(t))
         return transformation_matrix
 
     def decomp_essential_mat(self, E, q1, q2):
         """
-        Decompose the Essential matrix
-
-        Parameters
-        ----------
-        E (ndarray): Essential matrix
-        q1 (ndarray): The good keypoints matches position in i-1'th image
-        q2 (ndarray): The good keypoints matches position in i'th image
-
-        Returns
-        -------
-        right_pair (list): Contains the rotation matrix and translation vector
+        Функция разложиения существенной матрицы
+        Параметры:
+        E (ndarray): Существенная матрица
+        q1 (ndarray): Хорошие ключевые точки соответствия положения на i-1-м изображении
+        q2 (ndarray): Хорошие ключевые точки соответствия положения на i изображении
+        Возращает:
+        right_pair (list): Содержит матрицу поворота и вектор перемещения
         """
         def sum_z_cal_relative_scale(R, t):
-            # Get the transformation matrix
+            # Получение матрицы преобразования
             T = self._form_transf(R, t)
-            # Make the projection matrix
+            # Создание проекционной матрицы
             P = np.matmul(np.concatenate((self.K, np.zeros((3, 1))), axis=1), T)
 
-            # Triangulate the 3D points
+            # Триангуляция 3D-точек 
             hom_Q1 = cv2.triangulatePoints(self.P, P, q1.T, q2.T)
-            # Also seen from cam 2
             hom_Q2 = np.matmul(T, hom_Q1)
 
-            # Un-homogenize
+            # Не Гомогенизировать
             uhom_Q1 = hom_Q1[:3, :] / hom_Q1[3, :]
             uhom_Q2 = hom_Q2[:3, :] / hom_Q2[3, :]
 
-            # Find the number of points there has positive z coordinate in both cameras
+            # Поиск количества точек, имеющих положительную координату z в обеих камерах
             sum_of_pos_z_Q1 = sum(uhom_Q1[2, :] > 0)
             sum_of_pos_z_Q2 = sum(uhom_Q2[2, :] > 0)
 
-            # Form point pairs and calculate the relative scale
+            # Формирование пары точек и вычисление относительного масштаба
             relative_scale = np.mean(np.linalg.norm(uhom_Q1.T[:-1] - uhom_Q1.T[1:], axis=-1)/
                                      np.linalg.norm(uhom_Q2.T[:-1] - uhom_Q2.T[1:], axis=-1))
             return sum_of_pos_z_Q1 + sum_of_pos_z_Q2, relative_scale
 
-        # Decompose the essential matrix
+        # разложиение существенной матрицы
+        # R1, R2 - первая и вторая из возможных матриц вращения
+        # t - один из возможных вариантов перевода
         R1, R2, t = cv2.decomposeEssentialMat(E)
-        t = np.squeeze(t)
+        t = np.squeeze(t) #удаляется лишняя ось в массиве
 
-        # Make a list of the different possible pairs
+        # Составление списока различных возможных пар
         pairs = [[R1, t], [R1, -t], [R2, t], [R2, -t]]
 
-        # Check which solution there is the right one
+        # Определение правильного решения
         z_sums = []
         relative_scales = []
         for R, t in pairs:
@@ -206,7 +198,7 @@ class VisualOdometry():
             z_sums.append(z_sum)
             relative_scales.append(scale)
 
-        # Select the pair there has the most points with positive z coordinate
+        # Отбор пары, в которой больше всего точек с положительной координатой z
         right_pair_idx = np.argmax(z_sums)
         right_pair = pairs[right_pair_idx]
         relative_scale = relative_scales[right_pair_idx]
@@ -230,9 +222,10 @@ def main():
         else:
             q1, q2 = vo.get_matches(i)
             transf = vo.get_pose(q1, q2)
-            cur_pose = np.matmul(cur_pose, np.linalg.inv(transf))
+            cur_pose = np.matmul(cur_pose, np.linalg.inv(transf)) #вычисляем обратную матрицу transf и находим произведение с cur_pose
         gt_path.append((gt_pose[0, 3], gt_pose[2, 3]))
         estimated_path.append((cur_pose[0, 3], cur_pose[2, 3]))
+    # Отрисовка графиков
     plotting.visualize_paths(gt_path, estimated_path, "Visual Odometry", file_out=os.path.basename(data_dir) + ".html")
 
 
